091cd673c9764498241c60b89e1b01e8226dbbe1
lhj10340 <lhj10340@naver.com>
2024-06-03 17:36:23 +0900

    day05

--- java_lhj/src/Day04/homework/UpDownGame.java
+++ java_lhj/src/Day04/homework/UpDownGame.java
@@ -25,9 +25,25 @@ public class UpDownGame {
 		int i;
 		
 		// 이전에 했던 것 참고하여 일단 완료하긴 했다.
+		// do while 문이 적절하다. ( 최소 1번은 실행이 되어야 하니까. )
+		
+		// int num; ( 잘 모르겠으면 변수선언은 밖에다가 하면 맞다. )
+		
+		/* do {
+		 *  ( 정수 입력 : 이라는 안내 문구 출력 )
+		 *  ( 정수를 입력 )
+		 *  num = scan.nextInt();
+		 *  ( 정답 여부를 판별, DOWN 인지 UP 인지 정답인지 )
+		 *  : 잘 안되더라도 주석 쓰는 연습을 계속적으로 해야한다.
+		 *  ( num 가 random 보다 크면 down
+		 *    num 가 random 보다 작으면 up
+		 *    아니면 정답입니다 라고 출력.
+		 *  : 바로 코딩이 되면 좋지만, 안되면 주석으로 풀어쓰는 연습은 꼭 해야한다.
+		 * } while(random != num)
+		 * */
 		
 		for( ; ; ) {
-			System.out.println("정수 입력 : ");
+			System.out.print("정수 입력 : ");
 			i = scan.nextInt();
 			if (i==random) { 
 				System.out.println("정답입니다."); break; }
---
+++ java_lhj/src/day05/LabelEx01.java
@@ -0,0 +1,26 @@
+package day05;
+
+public class LabelEx01 {
+
+	public static void main(String[] args) {
+		int i, j;
+		for(i = 1; i<=5; i++) {
+			for(j = 1; j<=5; j++) {
+				System.out.print(i*j+ " ");
+			}
+			System.out.println();
+		}
+		// 라벨 이름 A 를 지정한 후, break A; 를 이용하면 라벨 이름이 A인 반복문을 빠져나갈 수 있다.
+		// 이런 형태도 있다는 정도만 알아둘 것, 거의 쓰이지 않는다.
+		A : for(i = 1; i<=5; i++) {
+			for(j = 1; j<=5; j++) {
+				System.out.print(i*j+ " ");
+				if (j ==3) {
+					break A;
+				}
+			}
+			System.out.println();
+	}
+
+}
+}
