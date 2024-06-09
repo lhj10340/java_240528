b34d9e79080414e149387c35d44f33eb9d90050b
lhj10340 <lhj10340@naver.com>
2024-06-09 23:34:42 +0900

    day08
    숙제 주석 달아보기

--- java_lhj/src/day08/homework/MethodEx01.java
+++ java_lhj/src/day08/homework/MethodEx01.java
@@ -1,5 +1,7 @@
 package day08.homework;
 
+import java.util.Scanner;
+
 public class MethodEx01 {
 
 	/* 정수 n 을 입력받아, 입력받은 n 크기를 가지는 배열을 생성하는 코드를 작성하세요.
@@ -7,13 +9,32 @@ public class MethodEx01 {
 	 * */
 
 	public static void main(String[] args) {
+		
+		//정수 입력
+		Scanner scan = new Scanner(System.in);
+		System.out.println("정수 입력 : ");
 
+		// 입력받은 정수를 이용하여 정수 배열을 생성
+		int n = scan.nextInt();
+		
+		
+		
 	}
+/** 기능 : 정수 n 을 입력하면(배열의 크기가 주어지면), n 의 크기를 가진 정수 배열이 생성된다.
+ *  매개변수 : 배열의 크기 => 정수 => int
+ *  리턴타입 : 정수 배열을 생성 => 정수 => int [] arr => int
+ *  메서드명 : createArray
+ * */
 
-	/** 기능 : 정수 n 의 크기를 입력하면, n 의 크기를 가진 정수 배열이 생성된다.
-	 *  매개변수 : 정수 n, n의 크기를 가지는 정수 배열 ( int n, int [] arr )
-	 *  리턴타입 : 정수 배열을 생성 => 정수 => int
-	 *  메서드명 : make
-	 * */
-	
+		public static int createArray(int n) {
+		
+		Scanner scan = new Scanner(System.in);
+		// 배열의 크기를 입력
+		n = scan.nextInt();
+		// 입력된 크기로 배열을 생성.
+		int [] arr = new int [n];
+		
+		return arr[n];
+
+}
 }
