9692a0fe32570812e2c9dd1c0c655741309ea728
lhj10340 <lhj10340@naver.com>
2024-06-17 17:47:53 +0900

    자바 3일차 복습
    IF ELSE 문을 활용한 홀짝 예제.

---
+++ study_lhj/src/Day03_study/IfEx02.java
@@ -0,0 +1,21 @@
+package Day03_study;
+
+public class IfEx02 {
+
+	public static void main(String[] args) {
+		// If else 문을 이용한 홀짝 판별 예제.
+		
+		int num = 7;
+		
+		if (num % 2 == 0) {
+			System.out.println("짝수입니다.");
+		}
+		else {
+			System.out.println("홀수 입니다.");
+		}
+		
+	}
+
+}
+
+// else 는 현위치에서 연결된 조건문이 거짓일 경우.
