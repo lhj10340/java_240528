af842c304157eda3f772f5f99f6737b5bf747b5d
lhj10340 <lhj10340@naver.com>
2024-06-18 16:54:29 +0900

    day15

---
+++ java_lhj/src/day15/SetEx02.java
@@ -0,0 +1,35 @@
+package day15;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class SetEx02 {
+
+	public static void main(String[] args) {
+		// 향상된 for 문을 이용하여 Set 에 저장된 값 가져오기.
+		
+		HashSet<Integer> set = new HashSet<Integer>();
+		set.add(1);
+		set.add(5);
+		set.add(9);
+		set.add(2);
+		set.add(3);
+		
+		for(Integer tmp : set) {
+			System.out.print(tmp);
+		}
+		
+		System.out.println();
+		
+		// Iterator 사용 예제.
+		
+		Iterator<Integer> it = set.iterator();
+		
+		while(it.hasNext()) {
+			Integer num = it.next();
+			System.out.print(num + " ");
+		}
+		
+	}
+
+}
