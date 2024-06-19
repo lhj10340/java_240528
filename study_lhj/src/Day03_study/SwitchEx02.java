b7542149fbbd36f54df2a4b8a1323277cace7d37
lhj10340 <lhj10340@naver.com>
2024-06-19 17:48:21 +0900

    자바 3일차 복습
    switch 예제 2번 진행 중

---
+++ study_lhj/src/Day03_study/SwitchEx02.java
@@ -0,0 +1,17 @@
+package Day03_study;
+
+import java.util.Scanner;
+
+public class SwitchEx02 {
+
+	public static void main(String[] args) {
+		
+		// 월을 입력받고, 계절을 출력하는 예제
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int num = scan.nextInt();
+		
+		}
+
+}
