96d398080ead1cdc4ed237e999f26cdc7d91752d
lhj10340 <lhj10340@naver.com>
2024-06-20 17:42:46 +0900

    자바 3일차 복습
    switch 2번 예제

--- study_lhj/src/Day03_study/SwitchEx02.java
+++ study_lhj/src/Day03_study/SwitchEx02.java
@@ -10,8 +10,26 @@ public class SwitchEx02 {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		int num = scan.nextInt();
+		System.out.println("월 입력 : ");
+		int month = scan.nextInt();
 		
+		switch(month) {
+		case 3,4,5:
+			System.out.println("봄 입니다.");
+			break;
+		case 6,7,8:
+			System.out.println("여름 입니다.");
+			break;
+		case 9,10,11:
+			System.out.println("가을 입니다.");
+			break;
+		case 12,1,2:
+			System.out.println("겨울 입니다.");
+			break;
+		default:
+			System.out.println("잘못된 월을 입력하였습니다.");
+			break;
+		}
 		}
 
 }
