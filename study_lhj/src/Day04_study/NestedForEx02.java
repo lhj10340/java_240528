96349a1cab639181191c4cd987caf32c5392ca08
lhj10340 <lhj10340@naver.com>
2024-06-25 17:49:08 +0900

    자바 4일차 복습
    중첩 for 문 예제 2번
    ( 갑자기 안 풀림 ... , 다시 고민 )

---
+++ study_lhj/src/Day04_study/NestedForEx02.java
@@ -0,0 +1,16 @@
+package Day04_study;
+
+public class NestedForEx02 {
+
+	public static void main(String[] args) {
+		
+		// num가 소수인지 아닌지 판별하는 코드를 이용하여 100이하의 소수를 출력하는 코드를 작성하세요.
+		
+		/* 반복횟수 : num 는 2부터 100까지 1씩 증가
+		 * 규칙성 : num 가 소수인지 아닌지 판별하는 코드를 활용해서 소수이면 num 를 출력, 아니면 아무것도 안함.
+		 * 반복문 종료 후 : 없음.
+		 * */
+		
+	}
+
+}
