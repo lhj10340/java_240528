1fb6a9e081d186b4f3432952cccadcac5223d1c0
lhj10340 <lhj10340@naver.com>
2024-06-28 17:19:54 +0900

    자바 5일차 복습
    * 출력 예제 모음

--- study_lhj/src/Day05_study/StarEx01.java
+++ study_lhj/src/Day05_study/StarEx01.java
@@ -11,6 +11,11 @@ public class StarEx01 {
 		 * *****
 		 * *****
 		 */
-
+		for( int i = 0; i < 5 ; i++ ) {
+			for( int j = 0; j < 5; j++ ) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 }
