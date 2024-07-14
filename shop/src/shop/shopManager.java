012ceeed41c50541d03ce8c70f67bb7e45313b12
lhj10340 <lhj10340@naver.com>
2024-07-15 07:09:45 +0900

    프로젝트 진행

--- shop/src/shop/shopManager.java
+++ shop/src/shop/shopManager.java
@@ -67,6 +67,27 @@ private Scanner scan = new Scanner(System.in);
 	private void checkBag() {
 		// 아이디 별로 등록된 상품의 리스트(장바구니) 및 구매의사 여부를 확인하는 메소드.
 		// 아이디 입력 시, 번호·상품명·가격 출력 후, 마지막으로 가격의 총액 및 구매하기 버튼 출력.
+		
+		// 검색
+		// 1. 이름 입력
+		System.out.print("이름 : ");
+		String search = scan.next();
+		// 2. 입력한 이름이 포함된 연락처들을 번호와 함께 출력
+		int i = 0, count = 0;
+		for (Contact contact : list) {
+			++i;;
+			if(contact.getName().contains(search)) {
+				count++;
+				System.out.println(i + ". " + contact);
+			}
+		}
+
+		//검색 결과가 없으면
+		if(count == 0) {
+			System.out.println("검색 결과가 없습니다.");
+			return;
+		}
+		
 	}
 
 	private void emptySome() {
@@ -74,6 +95,43 @@ private Scanner scan = new Scanner(System.in);
 		// 아이디 입력 시, 출력되는 내용은 checkBag() 와 동일.
 		// 삭제를 희망하는 상품의 번호를 입력하여 해당 상품의 삭제.
 		// 마지막으로 변동된 총액 출력.
+		
+		// 검색
+		
+		// 1. 이름 입력
+		System.out.print("이름 : ");
+		String search = scan.next();
+		
+		// 2. 입력한 이름이 포함된 들을 번호와 함께 출력
+		int i = 0, count = 0;
+		for (Contact contact : list) {
+			++i;;
+			if(contact.getName().contains(search)) {
+				count++;
+				System.out.println(i + ". " + contact);
+			}
+		}
+
+		//검색 결과가 없으면
+		if(count == 0) {
+			System.out.println("검색 결과가 없습니다.");
+			return;
+		}
+		
+		// 입력한 이름이 포함된 연락처들을 번호와 함께 출력
+		
+		// 번호를 선택
+		System.out.println("삭제할 번호 : ");
+		int index = scan.nextInt() - 1;
+		// 선택한 번호의 연락처를 삭제
+
+		if(list.remove(index) != null) {
+			System.out.println("연락처를 삭제했습니다.");
+			return;
+		}
+		System.out.println("연락처를 삭제하지 못했습니다.");
+	
+		
 	}
 
 	private void empty() {
