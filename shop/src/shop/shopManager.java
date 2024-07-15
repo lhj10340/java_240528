f6b4ad8a4b30e1ea90cf43a205b858121ebf4c04
lhj10340 <lhj10340@naver.com>
2024-07-15 09:03:29 +0900

    프로젝트 진행 중

--- shop/src/shop/shopManager.java
+++ shop/src/shop/shopManager.java
@@ -69,24 +69,33 @@ private Scanner scan = new Scanner(System.in);
 		// 아이디 입력 시, 번호·상품명·가격 출력 후, 마지막으로 가격의 총액 및 구매하기 버튼 출력.
 		
 		// 검색
-		// 1. 이름 입력
+		// 1. 아이디 입력
 		System.out.print("이름 : ");
 		String search = scan.next();
-		// 2. 입력한 이름이 포함된 연락처들을 번호와 함께 출력
+		// 2. 입력한 아이디에 저장된 리스트를 가격 (각각의 가격) 과 함께 출력
 		int i = 0, count = 0;
-		for (Contact contact : list) {
+		for ( ??? : list) {
 			++i;;
-			if(contact.getName().contains(search)) {
+			if(???.contains(search)) {
 				count++;
-				System.out.println(i + ". " + contact);
+				System.out.println(i + ". " + ???);
 			}
 		}
-
 		//검색 결과가 없으면
+		
 		if(count == 0) {
 			System.out.println("검색 결과가 없습니다.");
 			return;
 		}
+		
+		// 3. 2번과 함께 총액을 출력 후, 구매의사를 물어보는 안내문구 출력
+		
+		// 4. 구매의사
+		
+			// 4-1 예. 입력 시, ' 구매가 완료되었습니다. ' 출력
+		
+			// 4-2 아니오. 입력 시, ' 메뉴로 돌아갑니다. ' 출력 후, 메뉴로 ...
+
 		
 	}
 
@@ -98,17 +107,17 @@ private Scanner scan = new Scanner(System.in);
 		
 		// 검색
 		
-		// 1. 이름 입력
-		System.out.print("이름 : ");
+		// 1. 아이디 입력
+		System.out.print("아이디 : ");
 		String search = scan.next();
 		
-		// 2. 입력한 이름이 포함된 들을 번호와 함께 출력
+		// 2. 입력한 아이디에 저장된 리스트를 가격 (각각의 가격) 과 함께 출력
 		int i = 0, count = 0;
-		for (Contact contact : list) {
+		for ( ??? : list) {
 			++i;;
-			if(contact.getName().contains(search)) {
+			if( ??? .contains(search)) {
 				count++;
-				System.out.println(i + ". " + contact);
+				System.out.println(i + ". " + ??? );
 			}
 		}
 
@@ -118,18 +127,18 @@ private Scanner scan = new Scanner(System.in);
 			return;
 		}
 		
-		// 입력한 이름이 포함된 연락처들을 번호와 함께 출력
+		// 입력한 아이디에 저장된 리스트를 번호와 함께 출력
 		
 		// 번호를 선택
-		System.out.println("삭제할 번호 : ");
+		System.out.println("삭제할 물품의 번호 : ");
 		int index = scan.nextInt() - 1;
 		// 선택한 번호의 연락처를 삭제
 
 		if(list.remove(index) != null) {
-			System.out.println("연락처를 삭제했습니다.");
+			System.out.println("상품을 삭제했습니다.");
 			return;
 		}
-		System.out.println("연락처를 삭제하지 못했습니다.");
+		System.out.println("상품을 삭제하지 못했습니다.");
 	
 		
 	}
@@ -138,6 +147,35 @@ private Scanner scan = new Scanner(System.in);
 		// 아이디를 입력하여 상품의 리스트(장바구니) 확인 후, 전체를 삭제하는 메소드.
 		// 아이디 입력 시, 출력되는 내용은 checkBag() 와 동일.
 		// 안내문구 출력 후, 선택 시 전체 삭제처리.
+		
+		// 검색
+		
+		// 1. 아이디 입력
+		System.out.print("아이디 : ");
+		String search = scan.next();
+		
+		// 2. 입력한 아이디에 저장된 리스트를 가격 (각각의 가격) 과 함께 출력
+		int i = 0, count = 0;
+		for ( ??? : list) {
+			++i;;
+			if( ??? .contains(search)) {
+				count++;
+				System.out.println(i + ". " + ??? );
+			}
+		}
+
+		//검색 결과가 없으면
+		if(count == 0) {
+			System.out.println("검색 결과가 없습니다.");
+			return;
+		}
+		
+		// 3. 안내문구 출력 ( 비우기 여부 )
+		
+			// 3-1 예. 입력 시, ' 비우기가 완료되었습니다. ' 를 출력하며, 해당 아이디에 포함된 리스트를 전체 삭제.
+		
+			// 3-2 아니오. 입력 시, ' 메뉴로 돌아갑니다. ' 출력 후, 메뉴로 ...
+		
 	}
 
 	private void prev() {
