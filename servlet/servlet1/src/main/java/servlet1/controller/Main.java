a899e690b3e545661c81d0b70212ed6b57196c07
lhj10340 <lhj10340@naver.com>
2024-08-07 17:38:11 +0900

    Servlet_day01

--- servlet/servlet1/src/main/java/servlet1/controller/Main.java
+++ servlet/servlet1/src/main/java/servlet1/controller/Main.java
@@ -1,11 +1,14 @@
 package servlet1.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import servlet1.model.vo.Person;
 
 @WebServlet("/")
 public class Main extends HttpServlet {
@@ -14,14 +17,16 @@ public class Main extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) 
 					throws ServletException, IOException {
-		response.getWriter()
-		.append("Hello. 안녕하세요.");
-	}
+		// 화면에 홍길동이라는 이름을 전송
+		// name : 화면(jsp)에서 사용할 변수명
+		// 홍길동 : 화면에 보낼 이름
+		request.setAttribute("name", "홍길동");
 
-	protected void doPost(HttpServletRequest request, 
-			HttpServletResponse response) 
-					throws ServletException, IOException {
-		doGet(request, response);
+		Person p = new Person("임꺽정", 20);
+		request.setAttribute("person", p);
+		
+		// WEM-INF/views/main.jsp를 가져와서 화면에 전달
+		 request.getRequestDispatcher("/WEB-INF/views/main.jsp").forward(request, response);
 	}
 
 }
