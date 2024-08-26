5168df0f2320b2c697f52475e4af59a41028dd4e
lhj10340 <lhj10340@naver.com>
2024-08-26 11:47:32 +0900

    스프링 01일차 : DB 연동 및 테스트

--- spring projects/spring_lhj/src/main/java/kr/kh/spring/controller/HomeController.java
+++ spring projects/spring_lhj/src/main/java/kr/kh/spring/controller/HomeController.java
@@ -1,38 +1,31 @@
 package kr.kh.spring.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+import kr.kh.spring.service.MemberService;
+
+
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	// private MemberService memberservice = new MemberServiceImp();
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	// servlet 에서는 위와 같이 작성했다, spring 에서는 @Autowired 를 추가한 후, 아래와 같이 작성하면 된다.
+	
+	@Autowired
+	private MemberService memberService;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public String home(Model model) {
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+		String email = memberService.getEmail("abc123");
+		System.out.println(email);
 		return "home";
 	}
 	
