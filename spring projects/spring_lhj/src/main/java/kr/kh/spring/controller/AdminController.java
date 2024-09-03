0640d1e57a282d49874ba6e6d32c71c65bae5a8d
lhj10340 <lhj10340@naver.com>
2024-09-03 12:23:21 +0900

    스프링 07일차: interceptor 추가 및 적용

--- spring projects/spring_lhj/src/main/java/kr/kh/spring/controller/AdminController.java
+++ spring projects/spring_lhj/src/main/java/kr/kh/spring/controller/AdminController.java
@@ -54,7 +54,7 @@ public class AdminController {
 		return "/main/message";
 	}
 	
-	@PostMapping("/community/update")
+	@RequestMapping("/community/update")
 	public String communityUpdate(Model model, CommunityVO community) {
 		
 		boolean res = postService.updateCommunity(community);
---
+++ spring projects/spring_lhj/src/main/java/kr/kh/spring/interceptor/GuestInterceptor.java
@@ -0,0 +1,37 @@
+package kr.kh.spring.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import kr.kh.spring.model.vo.MemberVO;
+
+// 관리자만 관리자 페이지에 접근할 수 있께 하기 위해서 ... ( servlet 에서는 filter 를 이용해서 구현했다. )
+public class GuestInterceptor extends HandlerInterceptorAdapter{
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, 
+			HttpServletResponse response, 
+			Object handler)
+			throws Exception {
+			
+			// 로그인한 회원 정보를 가져온다.
+			MemberVO user = (MemberVO)request.getSession().getAttribute("user");
+		
+			// 로그인 했으면 메인페이지로 이동
+			if(user != null) {
+				
+				response.setContentType("text/html; charset=UTF-8");
+				response.getWriter().write("<script>alert(\"회원은 접근할 수 없습니다.\")</script>");
+				response.getWriter().write("<script>location.href=\"" + request.getContextPath() + "/" + "\"</script>");
+				response.flushBuffer();
+				
+				return false; // 지정한 경로로 이동시키기 위해 false 로 return 을 해야한다. 
+			}
+			// 아니면 그대로 진행한다.
+			return true;
+			
+	}
+	
+}
