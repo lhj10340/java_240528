f8fb8771f5a7a6c00751d7daf91561eb8f1dc875
lhj10340 <lhj10340@naver.com>
2024-09-10 11:27:14 +0900

    리액트 02일차 : 예제 - 버튼 컴포넌트

--- react/test1/src/App.js
+++ react/test1/src/App.js
@@ -15,8 +15,8 @@ function App() {
       {/* <Todo /> */}
       {/* <TodoList/> */}
       {/* <ButtonBox/> */}
-      <ButtonBox2/>
-      {/* <ButtonBox3/> */}
+      {/* <ButtonBox2/> */}
+      <ButtonBox3/>
     </div>
   );
 }
