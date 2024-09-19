fc801b014444d986d43419d1ca2e52945e2b7e8a
lhj10340 <lhj10340@naver.com>
2024-09-19 15:37:49 +0900

    리액트 05일차 : 스프링 연동 - 회원가입 연습 예제
    404 에러 발생 ( 사유 Signup.js 의 40번째 줄 ... fetch 부분 )

---
+++ spring projects/spring_3/src/main/java/kr/kh/spring_3/controller/TestController.java
@@ -0,0 +1,25 @@
+package kr.kh.spring_3.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.kh.spring_3.model.vo.MemberVO;
+import kr.kh.spring_3.service.MemberService;
+
+@RestController // @Controller + @ResponseBody
+@RequestMapping("/test")
+public class TestController {
+	
+	@Autowired
+	MemberService memberService;
+	
+	@PostMapping("/signup")
+	public boolean signup(@RequestBody MemberVO member) {
+		System.out.println(member);
+		return memberService.signup(member);
+	}
+	
+}
