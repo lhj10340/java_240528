c595cd7cebcc8c368aa89c86228f82c798bce9c0
lhj10340 <lhj10340@naver.com>
2024-09-19 17:01:25 +0900

    리액트 04,05일차 : 스프링과 리액트 연동 및 게시글 상세 조회 예제
    진행 중

---
+++ react/react2/src/App.js
@@ -0,0 +1,19 @@
+import Nav from "./Nav";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./Main";
+import PostList from "./PostList";
+import PostDetail from "./PostDetail";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Nav/>
+      <Routes>
+				<Route path={"/"} exact element={<Main/>}/>
+				<Route path={"/post/list/:co_num"} element={<PostList/>}/>
+        <Route path={"/post/detail/:po_num"} element={<PostDetail/>}/>
+			</Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
