162c9226725b611a308b8487d85aa6242e8f2e6d
lhj10340 <lhj10340@naver.com>
2024-09-24 16:08:02 +0900

    부트 1일차 : 프로젝트 생성, 기본설정 및 레이아웃 적용

---
+++ boot/src/main/java/kr/kh/boot/main/BootApplication.java
@@ -0,0 +1,15 @@
+package kr.kh.boot.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "kr.kh.boot")
+public class BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args);
+	}
+
+}
