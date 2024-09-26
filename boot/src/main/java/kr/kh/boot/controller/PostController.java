9df3563130349ba1999490e02873a679fb9f9536
lhj10340 <lhj10340@naver.com>
2024-09-26 16:11:29 +0900

    부트 03일차 : 게시글 등록/수정/삭제 구현 및 게시글 목록 보완

--- boot/src/main/java/kr/kh/boot/controller/PostController.java
+++ boot/src/main/java/kr/kh/boot/controller/PostController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import kr.kh.boot.model.vo.CommunityVO;
 import kr.kh.boot.model.vo.PostVO;
@@ -57,6 +59,55 @@ public class PostController {
 		model.addAttribute("post", post);
 		
 		return "post/detail";
+	}
+	
+	@GetMapping("/post/insert/{co_num}")
+	public String postInsert(@PathVariable int co_num) {
+		return "post/insert";
+	}
+	
+	@PostMapping("/post/insert")
+	public String postInsertPost(PostVO post) {
+		boolean res = postService.addPost(post);
+		System.out.println(post);
+		if(res) {
+			return "redirect:/post/list/"  + post.getPo_co_num();
+		}
+		return "redirect:/post/insert/" + post.getPo_co_num();
+	}
+	
+	@GetMapping("/post/update/{po_num}")
+	public String postUpdate(Model model, @PathVariable int po_num) {
+		
+		PostVO post = postService.getPost(po_num);
+		model.addAttribute("post", post);
+		
+		return "/post/update";
+	}
+	
+	@PostMapping("/post/update/{po_num}")
+	public String postUpdatePost(Model model, @PathVariable int po_num, PostVO post) {
+		
+		post.setPo_num(po_num);
+		boolean res = postService.updatePost(post);
+		
+		if(res) {
+			return "redirect:/post/detail/" + po_num;
+		}
+		return "redirect:/post/update/" + po_num;
+		
+	}
+	
+	@GetMapping("/post/delete/{co_num}/{po_num}")
+	public String postDelete(Model model, @PathVariable int po_num, @PathVariable int co_num) {
+		
+		boolean res = postService.deletePost(po_num);
+		
+		if(res) {
+			return "redirect:/post/list/" + co_num;
+		}
+		
+		return "redirect:/post/detail/" + po_num;
 	}
 	
 }
--- boot/src/main/java/kr/kh/boot/service/PostService.java
+++ boot/src/main/java/kr/kh/boot/service/PostService.java
@@ -20,6 +20,11 @@ public class PostService {
 	private PostDAO postDao;
 
 	public List<PostVO> getPostList(PostCriteria cri) {
+		
+		// 커뮤니티가 선택되지 않았다면, ( url 이 list/0 이라면  ... ) 아무것도 표시되지 않도록!
+		// 9/26 여기에 0 이라면 false 를 return 하도록 설정 .. ?
+		// list.html 에 강사님 풀이 작성해놓음, 일단 생각해본건 틀리긴 했다.
+		
 		return postDao.selectPostList(cri);
 	}
 
@@ -38,6 +43,39 @@ public class PostService {
 
 	public PostVO getPost(int po_num) {
 		return postDao.selectPost(po_num);
+	}
+
+	public boolean addPost(PostVO post) {
+		
+		if(post == null) {
+			return false;
+		}
+		
+		try {
+			return postDao.insertPost(post);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+	}
+
+	public boolean updatePost(PostVO post) {
+		
+		if(post == null) {
+			return false;
+		}
+		
+		try {
+			return postDao.updatePost(post);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public boolean deletePost(int po_num) {
+		return postDao.deletePost(po_num);
 	}
 
 }
